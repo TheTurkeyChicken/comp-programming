103c287b3b9c6f1d6848baa445276a6bad8fd0fe
TheTurkeyChicken <66879476+TheTurkeyChicken@users.noreply.github.com>
2021-07-17 13:02:44 -0700

    Update CCC10J5 (Knight Hop).cpp

--- DMOJ/CCC/CCC10J5 (Knight Hop).cpp
+++ DMOJ/CCC/CCC10J5 (Knight Hop).cpp
@@ -1,6 +1,39 @@
 #include <iostream>
+#include <queue>
 using namespace std;
 
+struct Data {
+	int r, c, moves;
+}start, stop;
+
+queue <Data> Q;
+int arr[8][8];
+bool vist[8][8];
+int rmove[] = {1, 2, 2, 1, -1, -2, -2, -1};
+int cmove[] = {2, 1, -1, -2, -2, -1, 1, 2};
+
+void BFS() {
+	while(!Q.empty()) {
+		Data cur = Q.front(); Q.pop();
+		if(cur.r == stop.r && cur.c == stop.c) {
+			cout << cur.moves;
+			return;
+		}
+		for(int i = 0; i < 8; i++) {
+			Data tcur;
+			tcur.r = cur.r + rmove[i];
+			tcur.c = cur.c + cmove[i];
+			tcur.moves = cur.moves + 1;
+			if(!vist[tcur.r][tcur.c])
+				Q.push(tcur);
+		}
+	}
+}
+
 int main() {
-	
+	cin >> start.r >> start.c >> stop.r >> stop.c;
+	Q.push(start);
+	vist[start.r][start.c] = true;
+	BFS();
+	return 0;
 }
