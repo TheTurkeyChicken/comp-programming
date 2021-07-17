a2170edc94d490fabeda98db1b1d4f94ffb52327
TheTurkeyChicken <66879476+TheTurkeyChicken@users.noreply.github.com>
2021-07-17 13:18:21 -0700

    Update CCC10J5 (Knight Hop).cpp

--- DMOJ/CCC/CCC10J5 (Knight Hop).cpp
+++ DMOJ/CCC/CCC10J5 (Knight Hop).cpp
@@ -12,9 +12,16 @@ bool vist[8][8];
 int rmove[] = {1, 2, 2, 1, -1, -2, -2, -1};
 int cmove[] = {2, 1, -1, -2, -2, -1, 1, 2};
 
+inline bool isValid(Data rc) {
+	if(rc.r >= 0 && rc.r < 8 && rc.c >= 0 && rc.c < 8 && !vist[rc.r][rc.c])
+		return 1;
+	return 0;
+}
+
 void BFS() {
 	while(!Q.empty()) {
 		Data cur = Q.front(); Q.pop();
+		vist[cur.r][cur.c] = 1;
 		if(cur.r == stop.r && cur.c == stop.c) {
 			cout << cur.moves;
 			return;
@@ -24,7 +31,7 @@ void BFS() {
 			tcur.r = cur.r + rmove[i];
 			tcur.c = cur.c + cmove[i];
 			tcur.moves = cur.moves + 1;
-			if(!vist[tcur.r][tcur.c])
+			if(isValid(tcur))
 				Q.push(tcur);
 		}
 	}
